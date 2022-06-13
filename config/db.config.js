b0e49102686deb0933cd8c457cc8b5704307ce98
Pablo Mateos <pmateos805@gmail.com>
2022-06-13 18:40:55 +0200

    feat: db config started

---
+++ config/db.config.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Products';
+
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.info(`Successfully connected to the database ${URI}`))
+  .catch((error) => {
+    console.error(`An error ocurred trying to connect to de database ${URI}`, error);
+    process.exit(0);
+  });
+
+process.on('SIGINT', function () {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected on app termination');
+    process.exit(0);
+  });
+});
