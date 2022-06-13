266a04f30c8d038b546f3901fbdd947d37d16daf
Pablo Mateos <pmateos805@gmail.com>
2022-06-13 21:01:22 +0200

    feat: product model

---
+++ models/Product.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product name is required"],
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required"],
+    },
+    image: {
+        type: String,
+        default: 'https://res.cloudinary.com/dfbloaduq/image/upload/v1655146665/Products-web/no-image_ydc2de.webp'
+    },
+    price: {
+        type: Number,
+        required: [true, "Price is required"],
+    },
+    size: {
+        type: String,
+        required: [true, "Size is required"],
+    },
+    used: {
+        type: Boolean,
+    }
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        delete ret.__v;
+        delete ret._id;
+        return ret;
+      },
+    },
+  }
+)
+
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
