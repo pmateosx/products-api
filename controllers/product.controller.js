92f2d97bbd8333d28b9f907206ee81fd67597c85
Pablo Mateos <pmateos805@gmail.com>
2022-06-14 00:41:24 +0200

    fix: space in controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -34,8 +34,6 @@ module.exports.getProductDetail = (req, res, next) => {
 
 module.exports.updateProduct = (req, res, next) => {
     Product.findByIdAndUpdate(req.params.id, req.body, { new: true })
-      .then(productUpdated => {
-        res.status(200).json(productUpdated)
-      })
-      .catch(next)
+        .then(productUpdated => res.status(200).json(productUpdated))
+        .catch(next)
 }
