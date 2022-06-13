470ef69ecae5715f6f2909e402cc20ed0d4fcffb
Pablo Mateos <pmateos805@gmail.com>
2022-06-14 00:59:16 +0200

    feat: cloudinary started

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const upload = require('./storage.config');
 
 // Controllers 
 const product = require('../controllers/product.controller')
@@ -9,7 +10,7 @@ router.get('/', (req, res, next) => res.status(200).json({ ok: 'yass!' }))
 
 //Product
 router.get('/products', product.listProducts)
-router.post('/products', product.createProduct)
+router.post('/products', upload.single('image'), product.createProduct)
 router.delete('/products/:id', product.deleteProduct)
 router.get('/products/:id', product.getProductDetail)
 router.patch('/products/:id', product.updateProduct)
